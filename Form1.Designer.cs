4856ca058599604556d9f02bc3c3db8a639e4152
Alexplayrus1 <87015375+Alexplayrus1@users.noreply.github.com>
2022-04-26 13:45:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Alex_s_lagswitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(153, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "lag on";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(153, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "lag off";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(374, 327);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Alex\'s LagSwitch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Alex_s_lagswitch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+     
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.X)
+            {
+
+                ProcessStartInfo internetoff = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C ipconfig /" + "release",
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+                Process.Start(internetoff);
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        
+        {
+            if (e.KeyCode == Keys.X)
+            {
+                ProcessStartInfo interneton = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C ipconfig /" + "renew",
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+                Process.Start(interneton);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo interneton = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = "/C ipconfig /" + "renew",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(interneton);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo internetoff = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = "/C ipconfig /" + "release",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(internetoff);
+        }
+    }
+}
